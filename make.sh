a037867aebd78190a4034c24d97a9dea78d7f8d2
Larry Liang <ptolemy428@gmail.com>
2015-08-11 14:21:55 +1000

    update make.sh

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ files=".vimrc .gitignore .gitconfig .vim"
 #create sym links
 for file in $files; do
   echo "Deleting symlink in home directory."
-  rm $HOME/$file
+  rm -rf $HOME/$file
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/$file
 done
